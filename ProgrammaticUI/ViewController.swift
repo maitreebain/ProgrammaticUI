c089cccdd4581364e5bace9780e6bf2e41a78076
maitreebain <maitreebain@gmail.com>
2020-01-28 11:22:23 -0500

    working on nav bar

--- ProgrammaticUI/ViewController.swift
+++ ProgrammaticUI/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         view.backgroundColor = .systemPurple
     }
-
-
+    
+    private func configureNavBar() {
+        //setting title for nav bar
+        navigationItem.title = "Programmatic UI"
+        //adding a UIBarbuttonItem to the nav bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: self, action: #selector(showSettings(_:)))
+    }
+    
+    @objc private func showSettings(_ sender: UIBarButtonItem) {
+        print("show settings")
+    }
+    
 }
 
