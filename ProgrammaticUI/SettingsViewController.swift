944c98691c187b5a2fea1bc4091efec4fab650c7
maitreebain <maitreebain@gmail.com>
2020-01-28 10:56:33 -0500

    Initial Commit

---
+++ ProgrammaticUI/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  ProgrammaticUI
+//
+//  Created by Maitree Bain on 1/28/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
---
+++ ProgrammaticUI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProgrammaticUI
+//
+//  Created by Maitree Bain on 1/28/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPurple
+    }
+
+
+}
+
